7581681dd8f39d03ee8740463f7a86ad074f5067
Celina Lind <lind.115@buckeyemail.osu.edu>
2021-08-08 22:46:46 -0400

    create basic file structure / create schema code needed for mysql database

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS manage_db;
+
+CREATE DATABASE manage_db;
+
+USE manage_db;
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id),
+    REFERENCES role(id),
+);
+
